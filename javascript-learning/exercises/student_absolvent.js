bdebe278da432c2573f72a858f3eac27a6df1fd8
Jiří Vondra <voji1@yahoo.com>
2025-07-27 10:50:25 +0200

    add folder javascript-learing

---
+++ javascript-learning/exercises/student_absolvent.js
@@ -0,0 +1,27 @@
+/*
+Cvičení ( ⌛10:00):
+Vytvořte program, který bude vyhodnocovat, jestli má student dostatek bodů k úspěšnému ukončení studia.
+Program ulož do složky: javascript-learning/exercises
+Název souboru: student_absolvent.js
+
+Vstupy: 
+Počet bodů studenta.
+Jméno studenta.
+
+Hodnocení:
+Minimální počet bodů pro úspěch: 75
+
+Výsledek zaloguj srozumitelně do konzole s jménem.
+
+*/
+
+const studentName = "Jiří Vondra";
+const result = 70;
+const minGradPoints =75;
+
+
+if (result >= minGradPoints) {
+    console.log(`Student ${studentName} has ${result} point and past test!`);
+} else {
+    console.error(`Student ${studentName} has ${result} point and do not pass in test!`);
+}
