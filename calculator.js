3d39079b3dc689c7740c9ae75614ffcbe1d56e2a
Jiří Vondra <voji1@yahoo.com>
2025-08-10 20:18:07 +0200

    complate homework

--- calculator.js
+++ calculator.js
@@ -20,3 +20,12 @@ Dělit 125 / 7
 Výsledek tvé práce napushuj do tvého repa a vytvoř Pull Request na Petra.
 */
 
+import { Calculator } from "./javascript-learning/objects/home-work/calculator.mjs";
+
+
+const calculator = new Calculator
+
+console.log("Sečíst 14 + 41 = " + calculator.add(14, 41));
+console.log("Odečíst 174 - 32 = " + calculator.subtract(174, 32));
+console.log("Násobit 47 * 50 = " + calculator.multiply(47, 50));
+console.log("Dělit 125 / 7 = " + calculator.divide(125, 7));
--- javascript-learning/objects/home-work/calculator.mjs
+++ javascript-learning/objects/home-work/calculator.mjs
@@ -1,4 +1,4 @@
-class Calculator {
+export class Calculator {
     add(a, b) {
         return a + b;
     }
@@ -18,10 +18,3 @@ class Calculator {
         return a / b;
     }
 }
-
-const calculator = new Calculator();
-
-console.log("Sečíst 14 + 41 = " + calculator.add(14, 41));
-console.log("Odečíst 174 - 32 = " + calculator.subtract(174, 32));
-console.log("Násobit 47 * 50 = " + calculator.multiply(47, 50));
-console.log("Dělit 125 / 7 = " + calculator.divide(125, 7));
